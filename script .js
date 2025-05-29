6c43112cbaf69d676c4a47a239bddc369d1a5d43
muhammadxoja2009 <karimovmuhammadxoja480@gmail.com>
2025-05-29 14:35:55 +0500

    Add files via upload

---
+++ script .js
@@ -0,0 +1,11 @@
+ 
+
+const body = document.getElementsByTagName("body")[0];
+body.style = " background-color:blue; "
+
+
+const h1 = document.createElement("h1")
+h1.textContent=prompt("Ism kiriting")
+h1.style = "color:yellow; font-size:70px; text-align:center; margin-top:250px"
+body.appendChild(h1)
+
